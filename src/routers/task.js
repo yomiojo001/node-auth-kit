24ffffb23f613b1bdc5cac7b02f0476399c522cb
Micheal Ojo <yomiojo001@gmail.com>
2020-04-23 23:57:59 +0100

    auth-kit-with-bcrypt

---
+++ src/routers/task.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const Task = require('../models/task');
+const auth = require('../middleware/auth')
+const router = new express.Router();
+
+
+
+
+router.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    });
+
+    try{
+        await task.save()
+        res.status(201).send(task)
+    }catch(e){
+        res.status(400).send(e)
+    }
+
+    
+})
+
+
+router.get('/tasks', auth, async (req, res) => {
+
+
+    try{
+        const task = await Task.find({owner: req.user._id})
+        res.send(task)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+
+
+router.get('/tasks/:id', auth, async (req, res) => {
+
+    const _id = req.params.id
+
+    try{
+        const task = await Task.findOne({_id, owner: req.user._id})
+        
+        if(!task){
+           return res.status(404).send()
+        }
+        res.send(task)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+router.patch('/tasks/:id', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperation){
+        res.status(400).send({error: 'Invalid Update!'})
+    }
+
+
+    try{
+        // const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true ,runValidators: true })
+        const task = await Task.findOne({_id: req.params.id, owner: req.user._id})
+
+        if(!task){
+         return   res.status(404).send()
+        }
+
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
+        res.send(task)
+        
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+
+router.delete('/tasks/:id', auth, async (req, res) =>{
+    try{
+        const task = await Task.findOneAndDelete({_id: req.params.id, owner: req.user._id})
+        if(!task){
+            res.status(404).send()
+        }
+        res.send(task)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+
+module.exports = router;
---
+++ src/routers/user.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+const router = new express.Router()
+
+
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
+})
+
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try{
+        const user = await User.findOne({_id})
+        res.send(user);
+        if(!user){
+          return  res.status(404).send()
+        }
+    }catch(e){
+        res.status(500).send(e)
+    }
+
+    
+})
+
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    
+    try{
+        await user.save()
+        const token = await user.generateAuthenticationToken()
+        res.status(201).send({ user, token })
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+router.post('/users/login', async (req, res) =>{
+    try{
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    const token = await user.generateAuthenticationToken()
+    
+    res.send({user, token})
+
+    }catch(e){
+        res.status(400).send()
+    }
+})
+
+router.post('/users/logout', auth, async(req,res) => {
+    try{
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+
+        await req.user.save()
+
+        res.send()
+
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+router.post('/users/logoutAll', auth, async(req, res) => {
+    try{
+        req.user.tokens = []
+
+        await req.user.save()
+
+        res.send()
+
+    }catch(e){
+        res.status(500).send()
+    }
+})
+
+router.patch('/users/me', auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isUpdateValidate = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isUpdateValidate){
+        res.status(400).send({error:'Invalid Update!'})
+    }
+
+
+    try{
+        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        
+        const user = await req.user
+        
+        updates.forEach((update) => user[update] = req.body[update])
+
+        await user.save()
+        
+        res.send(user)
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/users/me', auth, async (req, res) =>{
+
+    try{
+        await req.user.remove()
+        res.send(req.user)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+
+module.exports = router
